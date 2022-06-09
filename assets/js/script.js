78afc4ff8edf1cfa578c7b5380f6d7c6d0334a7c
Claire Cashmore <clairecashmore2017@gmail.com>
2022-06-09 17:12:59 -0400

    can go through if all right answers chosen

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@
 //Counter to distinguish which quiz questions we are on.
 var quizIdCounter = 0;
 var rightAnsInd = 0;
+var wrongAnsIndex = 0;
+
+var time = 0;
 // Variable for the event listener in main
 var pageContentEl = document.querySelector("#page-content");
 
@@ -9,9 +12,16 @@ var startBtnEl = window.document.querySelector("#start-quiz");
 //variable to listen to events in header
 
 var headerEl = document.querySelector("header");
+var resultSectionEl = document.querySelector("#result-section");
+var resultTextEl = document.querySelector("#result-text");
+
 
 var quizQuestionsEl = document.querySelector("#questions-list");
-
+//object to store into localStorage at the end of each quiz
+var scoreObj = {
+    time : 0,
+    percentage: 0
+};
 var questionObj = {
     questions: [
         "What HTML element do we put javascript into?", 
@@ -52,73 +62,127 @@ var randomNumber = function(min, max) {
   
     return value;
   };
-function rightAns(index){
-   //Change questions
-   document.querySelector("#prompt").textContent = questionObj.questions[index];
 
-
-   var listChoiceEl = document.createElement("li");
-   var listButtonEl = document.createElement("button");
- 
-   listButtonEl.textContent = questionObj.correctAns[index]; 
-   listButtonEl.className = "btn";
-   listChoiceEl.className = "question";
-   listChoiceEl.id = "question";
-
-   listChoiceEl.appendChild(listButtonEl);
-   quizQuestionsEl.appendChild(listChoiceEl);
-   
-   
-
-  }
-function wrongAns(index) {
-    
- 
-    
-    //mutliple choice
-    
-    
+// function to create multiple choice buttons
+function createMutlipleButtons(index, array,rightOrWrong){
     var listChoiceEl = document.createElement("li");
     var listButtonEl = document.createElement("button");
   
-    listButtonEl.textContent = questionObj.wrongAns[index]; 
+    listButtonEl.textContent = array[index]; 
     listButtonEl.className = "btn";
     listChoiceEl.className = "question";
-    listChoiceEl.id = "question";
-    // listChoiceEl.setAttribute("data-question-id", quizIdCounter);
-
+    if(rightOrWrong){
+    listButtonEl.id = "question-right";
+    }
+    else if(!rightOrWrong){
+        listButtonEl.id = "question-wrong";
+        listButtonEl.setAttribute("ans-id",quizIdCounter);
+        quizIdCounter++;
+    }
+   console.log(listButtonEl);
+ 
     listChoiceEl.appendChild(listButtonEl);
     quizQuestionsEl.appendChild(listChoiceEl);
+    
+    
+}
+function replaceAnswers(index, array,rightOrWrong,buttonId){
 
-    //
+    
+    if(rightOrWrong){
+        console.log("Replacing right button texts...");
+        var listButtonEl = document.querySelector("#question-right");
+        listButtonEl.textContent = array[index]; 
+        console.log(listButtonEl);
+    }
+    else if (!rightOrWrong){
+        console.log("Replacing wrong button at id: " + buttonId);
+        var listButtonEl = document.querySelector(".btn[ans-id='" + buttonId + "']");
+        console.log(listButtonEl);
+        listButtonEl.textContent = array[index]; 
+    }
 
+}
+
+function createNewQuestion(index,resultTextEl) {
+       //Change questions
+   document.querySelector("#prompt").textContent = questionObj.questions[index];
+    
+}
+
+function removeText(textToRemove) {
+    textToRemove.remove();
 }
 
 // function to deal with buttons in main
 function quizButtonHandler(event){
   var targetEl = event.target; 
+  var right = true;
+  var wrong = false;
+  
+  console.log(rightAnsInd);
   if(targetEl.matches("#start-quiz")){
 
-   
-
+   //**********start timer here*************
+   //every one second, increase our time to act as a timer.
+    // setInterval(function() {
+    //     time++;
+    //     console.log(time);
+    // }, 1000);
     // remove start prompt
     document.querySelector("#startExcerpt").remove();
 
     //remove start button
     startBtnEl.remove();
-    
-    rightAns(rightAnsInd);
+     
+    createNewQuestion(rightAnsInd,resultTextEl);
+    createMutlipleButtons(rightAnsInd, questionObj.correctAns,right);
     rightAnsInd++;
-    for(var i = 0; i < questionObj.questions.length-2; i++){
-        wrongAns(i,);
+    //use a for loop to create 3 wrong answers
+   
+    for(wrongAnsIndex; wrongAnsIndex < questionObj.questions.length-2; wrongAnsIndex++){
+        createMutlipleButtons(wrongAnsIndex, questionObj.wrongAns,wrong);
+     
     }
+    console.log(wrongAnsIndex);
+    
     }
-    //If it isnt a start button...
-    else if(target.matches("#question")){
-        console.log("have clicked a multiple choice answer");
+    else if(targetEl.matches("#question-right")){
+       
+
+        resultTextEl.textContent = "Correct!";
+
+        resultSectionEl.appendChild(resultTextEl);
+
+        console.log("have clicked a right answer");
+        
+      
+        //Next Question
+        replaceAnswers(rightAnsInd, questionObj.correctAns,right);
+        rightAnsInd++;
+        var newLimit = wrongAnsIndex+3;
+        var buttonId = 0;
+        console.log(wrongAnsIndex);
+        for( wrongAnsIndex; wrongAnsIndex < newLimit; wrongAnsIndex++) {
+            console.log("entering replace wrong answers for loop...");
+            replaceAnswers(wrongAnsIndex, questionObj.wrongAns,wrong,buttonId);  
+            console.log(questionObj.wrongAns[wrongAnsIndex]);
+            buttonId++;
+        }
+        
+
+    }
+    else if(targetEl.matches("#question-wrong")){
+        console.log(targetEl)
+     
+        resultTextEl.textContent = "Incorrect!"
+       
+        resultSectionEl.appendChild(resultTextEl);
+        console.log("have clicked a wrong answer");
+
+        //Next Question
     }
 };
-
 
 //function to deal with buttons in header
 function headerButtonHandler(event){
@@ -129,3 +193,17 @@ pageContentEl.addEventListener("click",quizButtonHandler);
 headerEl.addEventListener("click",headerButtonHandler);
 console.dir(startBtnEl);
 
+
+//PSEUDO CODE
+// create HTML
+// Start quiz
+    // Show first question
+// Evaluate button press
+    //Is it right or wrong
+        // tell them it's right or wrong, add to score, take away time if wrong
+        //display new questions
+            //replace old questions text.Content
+            //Repeat righ or wrog
+// Finish quiz 
+    //show final score
+        // timer and q's right
